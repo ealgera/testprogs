74ab5dbb812815ed210fb9aa600ffdf3d02ca3a9
Eric Algera <ericalgera@gmail.com>
2015-05-25 10:14:44 +0200

    Wijzigingen van les 7

--- readme.md
+++ readme.md
@@ -7,3 +7,6 @@ https://www.youtube.com/playlist?list=PLFE2CE09D83EE3E28
 Voor advanced:
 https://www.youtube.com/playlist?list=PLafPL3OjwKSI1xRvQJgsLmf8sCo1Hxkf8
 
+Voor GIT:
+http://rogerdudler.github.io/git-guide/
+
--- src/apples.java
+++ src/apples.java
@@ -1,12 +1,8 @@
+import java.util.Scanner;
+
 public class apples {
 	public static void main (String args[]) {
-		double pear;
-		
-		pear = 2.12;
-		System.out.println("Hello World");
-		System.out.print("Het getal is: ");
-		System.out.print(pear);
-		System.out.println(" . Goed he...?");		
+		Scanner mInput = new Scanner(System.in);
+		System.out.println("Waarde is " + mInput.nextLine());
 	}
-
 }
